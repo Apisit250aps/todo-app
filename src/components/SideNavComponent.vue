92e0033ecdaa1cbc644015b619c0a09cb60f2673
YUapisit <aps.apisit250@gmail.com>
2024-04-15 09:59:07 +0700

    setup

---
+++ src/components/SideNavComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav id="side-nav" v-show="isShow">
+        <div id="side-menu">
+            asd
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps(
+    {
+        isShow:Boolean,
+    }
+)
+
+</script>
+
+<style scoped>
+.link-m {
+    background-color: white;
+    padding: 1rem;
+    border-radius: 15px;
+
+}
+
+#side-nav {
+    height: 100dvh !important;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    transition: 0.5s;
+    background-color: #bababa;
+    padding: 1rem;
+    overflow: hidden;
+    
+}
+
+#side-menu {
+    /* width: 0; */
+    height: 100%;
+    transition: 0.5s;
+    background-color: white;
+    border-radius: 10px;
+    background: #e0e0e0;
+    box-shadow: 19px 19px 38px #bebebe,
+        -19px -19px 38px #ffffff;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SideNavComponent from '@/components/SideNavComponent.vue';
+</script>
+
+<template>
+  <main>
+    <SideNavComponent :is-show="true" />
+  </main>
+</template>
